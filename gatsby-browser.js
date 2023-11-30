c3f7a57f47bcfa335bd1367542fcb42d1a1e3119
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2023-11-30 16:01:19 +0100

    Added tailwind css.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,9 @@ import "@fontsource/montserrat/variable.css"
 import "@fontsource/merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
-// custom CSS styles
+// custom CSS styles -- probably don't need this, but will leave for now
 import "./src/style.css"
-
+// global css style
+import './src/styles/global.css'
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
